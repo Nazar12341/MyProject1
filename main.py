5428108ae17c944d122779dfa56243936dee007a
Nazar12341 <167625563+Nazar12341@users.noreply.github.com>
2024-05-11 17:32:47 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import time
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное",
-            "РОФЛ": "шутка",
-            "ЩИЩ": "одобрение или восторг",
-            "КРИПОВЫЙ": "страшный, пугающий",
-            "АГРИТЬСЯ": "злиться",
-            }
-print("Привет!")
-time.sleep(1)
-print("Если вам не понятно какое-то слово на молодёжном сленге, введите его ниже!")
-for i in range(5):
-    word = input("Введите непонятное слово (большими буквами!): ")
-    if word in meme_dict.keys():
-        print(meme_dict.get(word))
-    else:
-        print("К сожалению такого слова нет в списке! Введите другое слово или ждите обновлений.")
-    time.sleep(1)
+
+import discord
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run("MTIzNjMwMjY5ODk0OTY0MDIxNA.Gjffh-.YhOgE4Siri7XWjC5_kXSuVXn2hAHxlLuIVPcEg")
