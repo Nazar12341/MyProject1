13a50bde9b21fe58812f5af80cfa0720f9085d96
Nazar12341 <167625563+Nazar12341@users.noreply.github.com>
2024-05-18 13:29:20 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
-
 import discord
-from discord.ext import commands
 import random
+from discord.ext import commands
+from random import randint
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
-There are a number of utility commands being showcased here.'''
+pet = {
+    'health': 100,
+    'defence': 10,
+    'strength': 10
+}
 
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
+@bot.command('feed')
+async def feed(message):
+    pet['health'] +=10
+    pet['defence'] +=10
+    await message.send('Ваш питомец хорошо поел:' + str(pet))
 
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+@bot.command('train')
+async def train(message):
+    pet['health'] -=10
+    pet['defence'] +=10
+    pet['strength'] +=10
+    await message.send('Ваш питомец потренировался:' + str(pet))
 
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+@bot.command('fight')
+async def fight(message):
+    enemy = {
+    'health': randint(1,100),
+    'defence': randint(1,10),
+    'strength': randint(1,10)
+    }
+    await message.send('Бой начался')
+    while enemy['health'] >= 0 and pet['health'] >= 0:
+        pet['health'] -= enemy['strength'] - pet['defence']
+        enemy['health'] -= pet['strength'] - enemy['defence']
+        await message.send('ваш питомец:' + str(pet))
+        await message.send('ваш враг:' + str(pet))
+    if enemy['health'] > pet['health']:
+        await message.send('Вы проиграли')
+    else:
+        await message.send('Вы выйграли')
+        
+@bot.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Да, бот крутой.')
 
-bot.run("")
+
+bot.run("Token")
